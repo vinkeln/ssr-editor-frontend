00b79c3cfae7004e5228e351a03bde0dcfd0b0be
jacobLandefjall <jalf23@student.bth.se>
2025-09-22 14:58:47 +0200

    Added a target for localhost:1337, for the backend data.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337', // Backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
