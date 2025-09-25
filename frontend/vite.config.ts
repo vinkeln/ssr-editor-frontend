ef0018b0b9e33ae659f44801e28ad8a8a4a6f3aa
Linn <linn@MacBook-Air-som-tillhor-Linn.local>
2025-09-25 11:06:19 +0200

    Added a comma.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ssr-editor-frontend'
+  base: '/ssr-editor-frontend',
   server: {
     proxy: {
       '/api': {
