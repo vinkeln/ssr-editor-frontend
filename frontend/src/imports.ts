87cb4592d0d64ed46dfe75379769ce17332ab1e4
jacobLandefjall <jalf23@student.bth.se>
2025-09-30 15:31:31 +0200

    Made imports and export files, instead of having many imports in different files.

---
+++ frontend/src/imports.ts
@@ -0,0 +1,2 @@
+export { Header, Footer } from "./includes";
+export { HomePage, CreateDocs, SavedDocs, FilmsList, Register } from "./pages";
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from "./HomePage";
+export { default as CreateDocs } from "./CreateDocs";
+export { default as SavedDocs } from "./SavedDocs";
+export { default as FilmsList } from "./FilmsList";
+export { default as Register } from "./Register";
