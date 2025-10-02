f6442b8d985f113aa22229d753d9c5af6f07e53c
jacobLandefjall <jalf23@student.bth.se>
2025-10-02 08:45:54 +0200

    Added a image and improved the design for the homepage.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,26 @@
-
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import "../styles/HomePage.scss";
+import laptopDocs from "../images/laptop-docs.png";
 
 function HomePage() {
-
   return (
-    <>
+    <div className="home-container">
+      <div className="content">
+        <div className="text-section">
+          <h1>
+            Create DOCS: Free <br /> Online Document Creator.
+          </h1>
+          <Button 
+                component={Link}
+                to="/create" className="create-btn">Create a Document</Button>
+        </div>
 
-
-      <h1 className="home-title">Home Page</h1>
-
-    </>
+        <div className="image-section">
+          <img src={laptopDocs} alt="Laptop with document" />
+        </div>
+      </div>
+    </div>
   );
 }
 
