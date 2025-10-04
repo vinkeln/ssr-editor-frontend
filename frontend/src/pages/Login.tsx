d02b12440a13c1c4152fcdfe59d97184b79ae140
jacobLandefjall <jalf23@student.bth.se>
2025-10-04 12:55:04 +0200

    updated to make it more stylish and better classnames.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,10 @@
 import LoginForm from "../components/LoginForm";
+const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:1337/api/auth"; 
 
 export default function LoginPage() {
     return (
         <div className="login-page">
-            <LoginForm backendUrl="https://api.example.com/api" />
+            <LoginForm backendUrl={backendUrl} />
         </div>
     );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,10 +1,10 @@
 import RegisterForm from "../components/RegisterForm";
 
 export default function Register() {
-    
+    const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:1337/api/auth"; 
     return (
         <div className="register-page">
-            <RegisterForm backendUrl="https://backend.azurewebsites.net" /> {/* Change the url when deploying */}
+            <RegisterForm backendUrl={backendUrl} /> {/* Change the url when deploying */}
         </div>
     );
 }
