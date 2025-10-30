ffa54f10b4ac32adcb536af73b827df12a9770e4
jacobLandefjall <jalf23@student.bth.se>
2025-10-30 13:18:30 +0100

    Updated to hasrouter.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import './styles/index.css'
 import App from './App.tsx'
 
 // Basename is setting all routes under /ssr-editor-frontend
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/ssr-editor-frontend">
+    <HashRouter basename="/ssr-editor-frontend">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
